ee7bc6b70803db09219e8c6db06b711cbffdd4d9
HANA <45937565+yuhn126@users.noreply.github.com>
2019-04-17 12:14:57 +0900

    java

--- workspace/JavaApp/src/com/encore/j0417/StarTest.java
+++ workspace/JavaApp/src/com/encore/j0417/StarTest.java
@@ -3,7 +3,21 @@ package com.encore.j0417;
 public class StarTest {
 
 	public static void main(String[] args) {
+		for(int i = 0; i < 4; i++) {		// 			1줄 	2줄	 3줄	  4줄
+			for(int j = 0; j <= i; j++) {	// 별 출력 :   *   **  ***  ****
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 		
+		System.out.println("------");
+		
+		for(int i = 4; i > 0; i--) {		// 			1줄 	 2줄	  3줄   4줄
+			for(int j = 0; j < i; j++) {	// 별 출력 :  ****  ***  **  *
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 	}
 
 }
