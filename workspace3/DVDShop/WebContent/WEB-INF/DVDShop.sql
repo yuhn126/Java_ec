73cbba674d739acb60fff568f54cee589558f9b8
HANA <45937565+yuhn126@users.noreply.github.com>
2019-08-12 20:57:25 +0900

    0812

---
+++ workspace3/DVDShop/WebContent/WEB-INF/DVDShop.sql
@@ -0,0 +1,162 @@
+/* MOVIE */
+DROP TABLE Movie 
+	CASCADE CONSTRAINTS;
+
+/* MOVIE */
+CREATE TABLE Movie (
+	movieCode NUMBER NOT NULL, /* 영화코드 */
+	movieTitle VARCHAR2(50) NOT NULL, /* 영화제목 */
+	director VARCHAR2(20) NOT NULL, /* 감독 */
+	actors VARCHAR2(30) NOT NULL, /* 출연자 */
+	ganre VARCHAR2(30) NOT NULL, /* 장르 */
+	filmRate NUMBER DEFAULT 0 NOT NULL, /* 등급 */
+	openingDate DATE, /* 개봉일 */
+	runningTime NUMBER, /* 상영시간 */
+	stock NUMBER NOT NULL, /* 재고 */
+	image VARCHAR2(25) NOT NULL /* 영화 이미지 */
+);
+
+ALTER TABLE Movie
+	ADD
+		CONSTRAINT PK_Movie
+		PRIMARY KEY (
+			movieCode
+		);
+		
+
+		
+/* MEMBERSHIP */
+DROP TABLE Membership 
+	CASCADE CONSTRAINTS;
+
+/* MEMBERSHIP */
+CREATE TABLE Membership (
+	memberCode NUMBER NOT NULL, /* 회원번호 */
+	memberId VARCHAR2(20) NOT NULL, /* 회원아이디 */
+	memberPass VARCHAR2(20) NOT NULL, /* 비밀번호 */
+	memberName VARCHAR(10) NOT NULL, /* 회원이름 */
+	birth DATE NOT NULL, /* 생년월일 */
+	telNum VARCHAR2(15) NOT NULL, /* 연락처 */
+	rentalNum NUMBER NOT NULL /* 대여가능횟수 */
+);
+
+ALTER TABLE Membership
+	ADD
+		CONSTRAINT PK_Membership
+		PRIMARY KEY (
+			memberCode
+		);
+
+		
+		
+		
+/* RENTAL */
+DROP TABLE Rental 
+	CASCADE CONSTRAINTS;
+
+/* RENTAL */
+CREATE TABLE Rental (
+	rentalCode NUMBER NOT NULL, /* 대여번호 */
+	movieCode NUMBER NOT NULL, /* 영화코드 */
+	memberCode NUMBER NOT NULL, /* 회원번호 */
+	rentalDate DATE NOT NULL, /* 대여일 */
+);
+
+ALTER TABLE Rental
+	ADD
+		CONSTRAINT PK_Rental
+		PRIMARY KEY (
+			rentalCode
+		);
+
+ALTER TABLE Rental
+	ADD
+		CONSTRAINT FK_Movie_TO_Rental
+		FOREIGN KEY (
+			movieCode
+		)
+		REFERENCES Movie (
+			movieCode
+		);
+
+ALTER TABLE Rental
+	ADD
+		CONSTRAINT FK_Membership_TO_Rental
+		FOREIGN KEY (
+			memberCode
+		)
+		REFERENCES Membership (
+			memberCode
+		);
+		
+drop sequence rental_seq;
+create sequence rental_seq
+       start with 1001
+       increment by 1
+       nocycle
+       nocache;
+       
+ -- 시퀀스 쓸 때 : rental_seq.nextval
+       
+       
+       
+       
+drop trigger minus_renNum;
+drop trigger plus_renNum;
+drop trigger minus_stock;
+drop trigger plus_stock;
+       
+-- 트리거 (대여시 회원 대여 가능횟수 +-)
+--(대여)
+create or replace trigger minus_renNum
+after insert on Rental
+for each row
+begin
+	update Membership set rentalNum=rentalNum-1
+	where memberCode = :new.memberCode;
+end;
+/
+--(반납)
+create or replace trigger plus_renNum
+after delete on Rental
+for each row --행트리거
+begin
+	update Membership set rentalNum=rentalNum+1
+	where memberCode = :old.memberCode;
+end;
+/     
+
+
+-- 트리거(대여시 dvd 재고+-) 
+--(대여)
+create or replace trigger minus_stock
+after insert on Rental
+for each row --행트리거
+begin
+	update Movie set stock=stock-1
+	where movieCode = :new.movieCode;
+end;
+/
+
+--(반납)
+create or replace trigger plus_stock
+after delete on Rental
+for each row --행트리거
+begin
+	update Movie set stock=stock+1
+	where movieCode = :old.movieCode;
+end;
+/
+
+delete from MOVIE;
+delete from Membership;
+delete from RENTAL;
+
+insert into MOVIE (movieCode,movieTitle,director,ganre,filmRate,stock)
+values (19980231,'트루먼쇼(the truman Show)','피터 위어','드라마/로맨스',12,3);
+
+insert into Membership (memberCode, memberId, memberPass, memberName, birth, telNum, rentalNum)
+values (1234, 'gildong', 'a1234', '홍길동', '1992-11-11', '010-1111-1111', 3);
+
+insert into RENTAL (rentalCode, movieCode, memberCode, rentalDate, returnDate)
+values (rental_seq.nextval, 19980231, 1234, sysdate, sysdate+7);
---
+++ workspace3/DVDShop/src/com/encore/db/iba/MySqlMapClient.java
@@ -0,0 +1,33 @@
+package com.encore.db.iba;
+
+import java.io.IOException;
+import java.io.Reader;
+
+import com.ibatis.common.resources.Resources;
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.ibatis.sqlmap.client.SqlMapClientBuilder;
+
+public class MySqlMapClient {
+	/*
+	 * sqlMapConfig.xml문서(텍스트문서)를 객체화 시켜주는 클래스
+	 * 
+	 * ★SqlMapClient클래스 : XML내에 작성된 sql문을 호출!!
+	 */
+
+	private static final SqlMapClient sqlMap;
+
+	static {
+		try {
+			String resource = "./iba/sqlMapConfig.xml";
+			Reader reader = Resources.getResourceAsReader(resource);
+			sqlMap = SqlMapClientBuilder.buildSqlMapClient(reader);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException("MySqlMapConfig초기화 중 에러: " + e);
+		}
+	}// static
+
+	public static SqlMapClient getSqlMapInstance() {
+		return sqlMap;
+	}
+}
